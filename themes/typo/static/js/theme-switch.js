eda544d1130c2eae061b4f834ff5a9426ac9ac01
Alex Tsankov <antsankov@gmail.com>
2025-12-04 08:10:04 -0800

    fixed

--- themes/typo/static/js/theme-switch.js
+++ themes/typo/static/js/theme-switch.js
@@ -1,28 +1,14 @@
-function isAuto() {
-  return document.body.classList.contains("auto");
-}
-
 function setTheme() {
-  if (!isAuto()) {
-    return
+  // Default to dark mode
+  if (!document.body.classList.contains("light")) {
+    document.body.classList.remove("auto");
+    document.body.classList.add("dark");
   }
-
-  document.body.classList.remove("auto");
-  let cls = "light";
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    cls = "dark";
-  }
-
-  document.body.classList.add(cls);
 }
 
 function invertBody() {
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
-}
-
-if (isAuto()) {
-  window.matchMedia('(prefers-color-scheme: dark)').addListener(invertBody);
 }
 
 setTheme();
